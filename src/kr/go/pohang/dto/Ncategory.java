41383b2e7ddc959607b18363858f799c1102872c
leeda930920 <ldy1257@naver.com>
2023-05-30 20:27:34 +0900

    pro03

---
+++ src/kr/go/pohang/dto/Ncategory.java
@@ -0,0 +1,20 @@
+package kr.go.pohang.dto;
+
+public class Ncategory {
+	 private String ncate;
+	 private String ncatename;
+	public String getNcate() {
+		return ncate;
+	}
+	public void setNcate(String ncate) {
+		this.ncate = ncate;
+	}
+	public String getNcatename() {
+		return ncatename;
+	}
+	public void setNcatename(String ncatename) {
+		this.ncatename = ncatename;
+	}
+	 
+
+}
